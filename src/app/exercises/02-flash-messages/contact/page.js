20437e1cdec29c7d3d3a9297bb2ed4bee8eca7f5
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-17 17:14:15 -0400

    Flash messages solution

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,31 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+
+import { ToastContext } from '../../../../components/ToastProvider';
 
 function ContactPage() {
+  const router = useRouter();
+
+  const { createToast } =
+    React.useContext(ToastContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // TODO: Send the data to the server
+
+    router.push('/exercises/02-flash-messages');
+
+    createToast(
+      "Your message was received. We'll get back to you shortly!",
+      'success'
+    );
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import ToastProvider from '../../../components/ToastProvider';
+import ToastShelf from '../../../components/ToastShelf';
 import './styles.css';
 
 function FlashMsgLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ToastProvider>
+          {children}
+          <ToastShelf />
+        </ToastProvider>
+      </body>
     </html>
   );
 }
